6d234c37d5c035f9b95bae03cc7978e7a618eea3
Agasthian <agasthian_r@hotmail.com>
2020-01-21 23:49:59 +0530

    Created a backend server using express and mongo db.Compeleted the model, controller and routes configuration.

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+//Import route
+const userRoutes = require('./routes/user');
+
+//app
+const app = express();
+
+//DB
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//Route middleware
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} `);
+});
